ca57466678ae16355be9906228714f67e6be4265
ErikNils <erik1.nilsson2@gmail.com>
2022-01-07 01:39:53 +0100

    Now able to select sorting algorithm, sorting speed and change array size inside the program

---
+++ SortingAlgorithms/__init__.py
@@ -0,0 +1,24 @@
+from SortingAlgorithms.BubbleSort import bubbleSort
+from SortingAlgorithms.HeapSort import heapSort
+from SortingAlgorithms.InsertionSort import insertionSort
+from SortingAlgorithms.MergeSort import mergeSort
+from SortingAlgorithms.QuickSort import quickSort
+from SortingAlgorithms.SelectionSort import selectionSort
+
+alg_names = [
+        'Bubble Sort',
+        'Heapsort',
+        'Insertion Sort',
+        'Merge Sort',
+        'Quicksort',
+        'Selection Sort'
+    ]
+
+algorithms = [
+    bubbleSort,
+    heapSort,
+    insertionSort,
+    mergeSort,
+    quickSort,
+    selectionSort
+]
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 import pygame
+import pygame_widgets
 import random
-from SortingAlgorithms.BubbleSort import bubbleSort
-from SortingAlgorithms.HeapSort import heapSort
-from SortingAlgorithms.InsertionSort import insertionSort
-from SortingAlgorithms.MergeSort import mergeSort
-from SortingAlgorithms.QuickSort import quickSort
-from SortingAlgorithms.SelectionSort import selectionSort
+from SortingAlgorithms import alg_names, algorithms
+from colors import BLUE, WHITE, GREY, GREEN, CYAN, VIOLET, RED
+from pygame_widgets.button import Button
+from pygame_widgets.dropdown import Dropdown
+from pygame_widgets.slider import Slider
+
 
 pygame.font.init()
 
@@ -21,9 +22,7 @@ pygame.display.set_caption("Sorting Algorithm Visualizer")
 
 run = True
 
-BLUE = (0,0,255)
-WHITE = (255,255,255)
-GREY = (232,232,232)
+delay = 20
 
 array = []
 array_color = []
@@ -31,7 +30,7 @@ array_color = []
 
 
 # Generates an array of size n filled with random number bewteen 1 to n
-def generate_array(n):
+def generate_array(n=150):
     global array, array_color
     arr = []
     arr_color = []
@@ -40,13 +39,15 @@ def generate_array(n):
         arr_color.append(BLUE)
     array = arr
     array_color = arr_color
+    
+
 
 
 def refill():
     screen.fill(WHITE)
     draw()
     pygame.display.update()
-    pygame.time.delay(10)
+    pygame.time.delay(delay)
  
   
 # Draw the array values
@@ -68,24 +69,61 @@ def draw():
     for i in range(0, len(array)):
         pygame.draw.line(screen, array_color[i], (bar_space * i+bar_width, 100),
             (bar_space * i+bar_width, array[i]*bar_height + 100), bar_width)
+
+
+alg_dropdown = Dropdown(
+    screen, 200, 20, 130, 50, name='Select Algorithm',
+    choices=alg_names,
+    borderRadius=3, colour=VIOLET, values=algorithms, direction='down', textHAlign='left'
+)
         
-        
+
+def pygame_sort():
+    try:
+        alg_dropdown.getSelected()(array,array_color,refill)
+    except Exception:
+        None
+    
+
+
+sort_button = Button(
+    screen, 385, 20, 80, 50, text='Sort!', fontSize=30,
+    margin=20, inactiveColour=RED, pressedColour=GREEN,
+    radius=5, onClick=pygame_sort, font=pygame.font.SysFont('calibri', 15),
+    textVAlign='bottom'
+)
+
+
+delay_slider = Slider(screen, 350, 75, 150, 10, min=5, max=200, step=2)
+
+generate_slider = Slider(screen, 30, 75, 150, 10, min=5, max=200, step=1)
+
+
+def pygame_generate():
+    generate_array(generate_slider.getValue())
+
+gen_button = Button(
+    screen, 50, 20, 110, 50, text='Generate Array', fontSize=30,
+    margin=20, inactiveColour=CYAN, pressedColour=GREEN,
+    radius=5, onClick=pygame_generate, font=pygame.font.SysFont('calibri', 15),
+    textVAlign='bottom'
+)
+
 
 
 if __name__ == '__main__':
     # Needs to have generated an array before function calls to stop it from crashing
     generate_array(20)
     while run:
-        screen.fill((255, 255, 255))
-        for event in pygame.event.get():
+        screen.fill(WHITE)
+        delay = 205 - delay_slider.getValue()
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    generate_array(150) 
-                if event.key == pygame.K_RETURN:
-                    quickSort(array,array_color,refill)
+            
         draw()
+        pygame_widgets.update(events)
         pygame.display.update()
 
     pygame.quit()
